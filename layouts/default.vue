593a683caed08789cf32b9ac9b824bf3470e6483
joxmal <joxmal123@gmail.com>
2024-01-16 02:29:01 -0400

    se añadieron las tecnologias en los proyectos 4

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
-<div class="hidden dark:block fixed top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-<div class="dark:hidden fixed inset-0 -z-10 h-screen w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_30%,#63E000_100%)]"></div>
+<!-- <div class="hidden dark:block fixed inset-0 -z-10 h-screen w-screen items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> -->
+<!-- <div class="hidden dark:block fixed top-0 z-[-2] h-screen w-screen bg-blue-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div> -->
+<div class="fixed inset-0 -z-10 h-screen w-screen dark:items-center dark:px-5 dark:py-24 bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_30%,#0EC7EC_100%)] dark:bg-transparent dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#408CD3_100%)] transition-colors duration-1000"></div>
 
-<div class="py-5 max-w-[1120px] h-full text-black dark:text-white mx-auto px-4 dark:bg-transparent bg-gray-500/10 ">
+<div class="py-5 max-w-[1120px] h-full text-black dark:text-white mx-auto px-4 ">
     <Header class=""/>
     <div class="flex flex-col justify-center ">
         <NuxtPage/>
@@ -23,7 +24,6 @@ html {
 
 
 body{
-    @apply dark:bg-black bg-sky-200 ;
     font-family: 'Onest Variable', sans-serif;
 }
 
